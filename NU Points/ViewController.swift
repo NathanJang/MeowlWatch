37070d79040e01004d76983b4d36e0818f9f2882
Jonathan Chan <jonchanyc@outlook.com>
2017-03-17 21:02:23 -0500

    Initial Commit

---
+++ NU Points/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NU Points
+//
+//  Created by Jonathan Chan on 2017-03-17.
+//  Copyright © 2017 Jonathan Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
