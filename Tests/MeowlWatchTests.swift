03a9da3bee5df2b42b92b0d2ba3eb526f84a543d
Jonathan Chan <jonchanyc@outlook.com>
2019-03-01 15:45:14 -0600

    Add empty test

---
+++ Tests/MeowlWatchTests.swift
@@ -0,0 +1,13 @@
+//
+//  MeowlWatchTests.swift
+//  MeowlWatchTests
+//
+//  Created by Jonathan Chan on 2019-03-01.
+//  Copyright © 2019 Jonathan Chan. All rights reserved.
+//
+
+import XCTest
+
+class MeowlWatchTests: XCTestCase {
+
+}
